569a809d3fc8a99eaf4c2c4f528682fa55dc5ee7
Barbara Śmiech <b.smiech@wp.pl>
2017-08-16 09:26:38 +0200

    prompt

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-var a = 10;
-var h = 12;
+var a = prompt('Podaj długość podstawy trójkata');
+var h = prompt('Podaj wysokość trójkąta');
 var triangleArea = a * h / 2;
+alert('Pole trójkata wynosi ' +triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
